b367b8481bf3d5b47d7b7106dcb5e1ac3821d083
balintvecsey <balint.vecsey@gmail.com>
2017-07-06 12:17:25 +0200

    add a simple header for the menu

--- app/src/main/java/com/greenfox/paranoidandroids/fidgetcubeapp/MainActivity.java
+++ app/src/main/java/com/greenfox/paranoidandroids/fidgetcubeapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.greenfox.paranoidandroids.fidgetcubeapp;
 
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
