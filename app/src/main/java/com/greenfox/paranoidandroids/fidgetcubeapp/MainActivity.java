7b8a09dc0fbedde195d827a262029d43e8671973
Nagy Dora <dorinagy06@gmail.com>
2017-07-06 12:27:04 +0200

    change action bar icon, delete ic_launcher icons

--- app/src/main/java/com/greenfox/paranoidandroids/fidgetcubeapp/MainActivity.java
+++ app/src/main/java/com/greenfox/paranoidandroids/fidgetcubeapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.greenfox.paranoidandroids.fidgetcubeapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ActionBar actionBar = getSupportActionBar();
+    actionBar.setDisplayUseLogoEnabled(true);
+    actionBar.setDisplayShowHomeEnabled(true);
   }
 }
